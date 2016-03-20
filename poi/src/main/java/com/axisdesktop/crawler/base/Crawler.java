e58e4473853d9df76b3f6244cd5e4aeb2cf0e7c6
fxcoder <barabass@gmail.com>
2016-03-21 01:12:51 +0200

    jj

--- poi/src/main/java/com/axisdesktop/crawler/base/Crawler.java
+++ poi/src/main/java/com/axisdesktop/crawler/base/Crawler.java
@@ -1,5 +1,14 @@
 package com.axisdesktop.crawler.base;
 
-public interface Crawler {
+import java.net.HttpURLConnection;
+import java.net.Proxy;
 
+public interface Crawler extends Runnable {
+	Proxy getProxy();
+
+	HttpURLConnection getConnection();
+
+	String getUserAgent();
+
+	String getReferer();
 }
