e532195553e52758ea5e99f3643959b009bc8514
fxcoder <barabass@gmail.com>
2016-04-19 00:06:09 +0300

    oo

--- poi/src/main/java/com/axisdesktop/user/SecurityUtil.java
+++ poi/src/main/java/com/axisdesktop/user/SecurityUtil.java
@@ -1,18 +1,18 @@
-package com.axisdesktop.user;
-
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-
-public class SecurityUtil {
-
-	public static void logInUser( AppUser user ) {
-		AppUser userDetails = new AppUser.Builder().firstName( user.getFirstName() ).id( user.getId() )
-				.lastName( user.getLastName() ).password( user.getPassword() ).role( user.getRole() )
-				.socialSignInProvider( user.getSocialSignInProvider() ).username( user.getEmail() ).build();
-
-		Authentication authentication = new UsernamePasswordAuthenticationToken( userDetails, null,
-				userDetails.getAuthorities() );
-		SecurityContextHolder.getContext().setAuthentication( authentication );
-	}
-}
+// package com.axisdesktop.user;
+//
+// import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+// import org.springframework.security.core.Authentication;
+// import org.springframework.security.core.context.SecurityContextHolder;
+//
+// public class SecurityUtil {
+//
+// public static void logInUser( AppUser user ) {
+// AppUser userDetails = new AppUser.Builder().firstName( user.getFirstName() ).id( user.getId() )
+// .lastName( user.getLastName() ).password( user.getPassword() ).role( user.getRole() )
+// .socialSignInProvider( user.getSocialSignInProvider() ).username( user.getEmail() ).build();
+//
+// Authentication authentication = new UsernamePasswordAuthenticationToken( userDetails, null,
+// userDetails.getAuthorities() );
+// SecurityContextHolder.getContext().setAuthentication( authentication );
+// }
+// }
