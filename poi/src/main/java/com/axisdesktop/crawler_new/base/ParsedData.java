5d25a12979a7a7b2c285eecd8bd87b9ac0cbf526
fxcoder <barabass@gmail.com>
2017-09-12 22:54:32 +0300

    i

--- poi/src/main/java/com/axisdesktop/crawler_new/base/ParsedData.java
+++ poi/src/main/java/com/axisdesktop/crawler_new/base/ParsedData.java
@@ -6,4 +6,21 @@ import java.util.HashSet;
 public class ParsedData {
 	private HashSet<String> links = new HashSet<>();
 	private HashMap<String, Object> data = new HashMap<>();
+
+	public HashSet<String> getLinks() {
+		return links;
+	}
+
+	public void setLinks( HashSet<String> links ) {
+		this.links = links;
+	}
+
+	public HashMap<String, Object> getData() {
+		return data;
+	}
+
+	public void setData( HashMap<String, Object> data ) {
+		this.data = data;
+	}
+
 }
