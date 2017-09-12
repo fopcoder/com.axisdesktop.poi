c88c6ba4812acfc2b08063413476d4b1a9dce4e6
fxcoder <barabass@gmail.com>
2017-09-12 22:51:11 +0300

    i

--- poi/src/main/java/com/axisdesktop/crawler_new/base/ParsedData.java
+++ poi/src/main/java/com/axisdesktop/crawler_new/base/ParsedData.java
@@ -1,9 +1,9 @@
 package com.axisdesktop.crawler_new.base;
 
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 
 public class ParsedData {
-	private HashSet links;
-	private Map data;
+	private HashSet<String> links = new HashSet<>();
+	private HashMap<String, Object> data = new HashMap<>();
 }
