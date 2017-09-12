229a6c13986f093728777fbc5acb4ffacde94584
fxcoder <barabass@gmail.com>
2017-09-12 23:34:15 +0300

    jj

---
+++ poi/src/main/java/com/axisdesktop/crawler_new/impl/DorogaUaParser.java
@@ -0,0 +1,19 @@
+package com.axisdesktop.crawler_new.impl;
+
+import com.axisdesktop.crawler_new.base.ParsedData;
+import com.axisdesktop.crawler_new.base.Parser;
+
+public class DorogaUaParser implements Parser {
+	private String raw;
+
+	public DorogaUaParser( String raw ) {
+		this.raw = raw;
+	}
+
+	@Override
+	public ParsedData parse() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
